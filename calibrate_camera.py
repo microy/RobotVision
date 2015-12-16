ffc1e7973001dd48b8695739c2aec7352133311f
Michaël Roy <microygh@gmail.com>
2015-12-16 14:50:17 +0100

    Add chessboard square size, and change calibration flags.

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -11,35 +11,37 @@ import pickle
 import numpy as np
 import cv2
 
-# Calibration pattern size
+# Chessboard pattern size
 pattern_size = ( 9, 6 )
-# Chessboard pattern
+# Chessboard pattern points
 pattern_points = np.zeros( ( np.prod( pattern_size ), 3 ), np.float32 )
 pattern_points[ :, :2 ] = np.indices( pattern_size ).T.reshape( -1, 2 )
+# Chessboard square size (in mm)
+pattern_points *= 34.15
 # 3D points
 object_points = []
 # 2D points
 image_points = []
 # Get the chessboard image files
 image_files = glob.glob( 'calibration-*.png' )
+# Chessboard detection flags
+flags  = 0
+flags |= cv2.CALIB_CB_ADAPTIVE_THRESH
+flags |= cv2.CALIB_CB_NORMALIZE_IMAGE
+# Termination criteria for the corner detection
+criteria = ( cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 1e-5 )
 # Find the chessboard on each image
 for filename in image_files :
 	# Load the image
 	image = cv2.imread( filename )
 	# Convert the image in grayscale
 	image = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
-	# Chessboard detection flags
-	flags  = 0
-	flags |= cv2.CALIB_CB_ADAPTIVE_THRESH
-	flags |= cv2.CALIB_CB_NORMALIZE_IMAGE
 	# Find the chessboard corners on the image
 	found, corners = cv2.findChessboardCorners( image, pattern_size, flags = flags )
 	# Pattern not found
 	if not found :
 		print( 'Chessboard not found on image {}...'.format( filename ) )
 		continue
-	# Termination criteria for the corner detection
-	criteria = ( cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 30, 1e-5 )
 	# Refine the corner positions
 	cv2.cornerSubPix( image, corners, ( 11, 11 ), ( -1, -1 ), criteria )
 	# Store image and corner informations
@@ -51,12 +53,14 @@ image_size = cv2.imread( image_files[ 0 ] ).shape[ :2 ][ ::-1 ]
 flags  = 0
 #flags |= cv2.CALIB_USE_INTRINSIC_GUESS
 #flags |= cv2.CALIB_FIX_PRINCIPAL_POINT
-flags |= cv2.CALIB_FIX_ASPECT_RATIO
+#flags |= cv2.CALIB_FIX_ASPECT_RATIO
 #flags |= cv2.CALIB_ZERO_TANGENT_DIST
 flags |= cv2.CALIB_RATIONAL_MODEL
+#flags |= cv2.CALIB_FIX_K1
+#flags |= cv2.CALIB_FIX_K2
 #flags |= cv2.CALIB_FIX_K3
-flags |= cv2.CALIB_FIX_K4
-flags |= cv2.CALIB_FIX_K5
+#flags |= cv2.CALIB_FIX_K4
+#flags |= cv2.CALIB_FIX_K5
 # Camera calibration
 calibration = cv2.calibrateCamera( object_points, image_points, image_size, flags = flags )
 # Store the calibration results in a dictionary
